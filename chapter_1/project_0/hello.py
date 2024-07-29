b50c8d60f9a091c934876aba49925191166adcb1
Veronelli <facu2000veronelli@outlook.com>
2024-07-29 09:22:40 -0300

    add ini config

--- chapter_1/project_0/hello.py
+++ chapter_1/project_0/hello.py
@@ -1,10 +1,18 @@
 import sys
-
+from django.urls import re_path
+from django.http import HttpResponse
+from django.core.wsgi import get_wsgi_application
 from django.conf import settings
+from configparser import ConfigParser
+
+config_parser = ConfigParser()
+config_parser.read('settings.ini')
+
+DEBUG = True if config_parser.get('django', 'DEBUG') == 'True' else False
 
 settings.configure(
-    DEBUG=True,
-    SECRET_KEY='thisisthesecretkey',
+    DEBUG=DEBUG,
+    SECRET_KEY=config_parser.get('django', 'SECRET_KEY'),
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
@@ -13,8 +21,7 @@ settings.configure(
     ),
 )
 
-from django.urls import re_path
-from django.http import HttpResponse
+application = get_wsgi_application()
 
 def index(request):
     return HttpResponse('Hello World')
