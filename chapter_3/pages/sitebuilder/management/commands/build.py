aa71e5f0c2f19cb1ba1477337625c28be59b84cb
Veronelli <facu2000veronelli@outlook.com>
2024-08-13 09:01:50 -0300

    build static file using command management

--- chapter_3/pages/sitebuilder/management/commands/build.py
+++ chapter_3/pages/sitebuilder/management/commands/build.py
@@ -2,10 +2,11 @@ import os
 import shutil
 from django.conf import settings
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError, CommandParser
 from django.urls import reverse
 from django.test.client import Client
-
+from os.path import join
+ 
 
 def get_pages():
     for name in os.listdir(settings.SITE_PAGES_DIRECTORY):
@@ -16,8 +17,28 @@ def get_pages():
 class Command(BaseCommand):
     help = "Build static site output."
 
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "files",
+            type=list[str],
+            nargs="*",
+            help="Pages to build"
+        )
+
     def handle(self, *args, **options):
         """Request pages and build output."""
+        if args:
+            pages = args
+            available = (get_pages())
+            invalid = []
+            
+            for page in pages:
+                if page not in available:
+                    invalid.append(page)
+            if invalid:
+                msg = "Invalid pages: {}".format(', ', join(invalid))
+                raise CommandError(msg)
+                        
         if os.path.exists(settings.SITE_OUTPUT_DIRECTORY):
             shutil.rmtree(settings.SITE_OUTPUT_DIRECTORY)
         os.mkdir(settings.SITE_OUTPUT_DIRECTORY)
@@ -27,10 +48,11 @@ class Command(BaseCommand):
         for page in get_pages():
             url = reverse("page", kwargs={"slug": page})
             response = client.get(url)
-        if page == "index":
-            output_dir = settings.SITE_OUTPUT_DIRECTORY
-        else:
-            output_dir = os.path.join(settings.SITE_OUTPUT_DIRECTORY, page)
-            os.makedirs(output_dir)
-        with open(os.path.join(output_dir, "index.html"), "wb") as f:
-            f.write(response.content)
+            if page == "index":
+                output_dir = settings.SITE_OUTPUT_DIRECTORY
+            else:
+                output_dir = os.path.join(settings.SITE_OUTPUT_DIRECTORY, page)
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+            with open(os.path.join(output_dir, "index.html"), "wb") as f:
+                f.write(response.content)
