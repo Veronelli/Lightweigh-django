1e95013606ac9933329687195ae3d48526293ff5
facu <facu@facus-Air.fibertel.com.ar>
2024-09-30 08:56:51 -0300

    add migration

--- chapter_4/django-rest-framework/scrum/scrum/urls.py
+++ chapter_4/django-rest-framework/scrum/scrum/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path(r'^api/token/', view=obtain_auth_token, name="api-token")
+    re_path(r'^api/token/', view=obtain_auth_token, name="api-token")
 ]
