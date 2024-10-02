9a6eb4ff2bc24aeab780ac137075d6fd93c9902b
facu <facu@facus-Air.fibertel.com.ar>
2024-10-02 09:56:56 -0300

    add links

--- chapter_4/django-rest-framework/scrum/scrum/urls.py
+++ chapter_4/django-rest-framework/scrum/scrum/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from board.url import router
 
 urlpatterns = [
-    re_path(r'^api/token/', view=obtain_auth_token, name="api-token")
+    re_path(r'^api/token/', view=obtain_auth_token, name="api-token"),
+    re_path(r'^api/', include(router.urls)),
 ]
